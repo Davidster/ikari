b1e086a16fd57d88b8eceed82e6c3ab1026f51ca
David Huculak <davidhuculak5@gmail.com>
2022-06-04 12:33:41 -0400

    dummy

--- src/gltf_loader.rs
+++ src/gltf_loader.rs
@@ -8,11 +8,13 @@ use gltf::Gltf;
 pub struct GltfVertex {
     position: [f32; 3],
     normal: Option<[f32; 3]>,
+    tex_coord: Option<[f32; 2]>,
 }
 
 #[repr(C)]
 #[derive(Clone, Debug, PartialEq)]
 pub struct GltfMesh {
+    name: Option<String>,
     vertices: Vec<GltfVertex>,
     indices: Option<Vec<u16>>,
     instance_transforms: Vec<Matrix4<f32>>,
@@ -35,6 +37,7 @@ fn gltf_transform_to_mat4(gltf_transform: gltf::scene::Transform) -> Matrix4<f32
     }
 }
 
+// TODO: return result instead of panic
 pub fn load_gltf(path: &str) -> Result<Vec<GltfMesh>> {
     let (document, buffers, images) = gltf::import(path)?;
 
@@ -63,23 +66,46 @@ pub fn load_gltf(path: &str) -> Result<Vec<GltfMesh>> {
                 &buffer[byte_range]
             };
 
-            let vertices: &[[f32; 3]] = first_triangle
+            let positions: &[[f32; 3]] = first_triangle
                 .attributes()
                 .find(|(semantic, _)| *semantic == gltf::Semantic::Positions)
                 .map(|(_, accessor)| {
                     let data_type = accessor.data_type();
+                    let dimensions = accessor.dimensions();
+                    if dimensions != gltf::accessor::Dimensions::Vec3 {
+                        panic!("Expected vec3 data but found: {:?}", dimensions);
+                    }
                     if data_type != gltf::accessor::DataType::F32 {
                         panic!("Expected f32 data but found: {:?}", data_type);
                     }
                     bytemuck::cast_slice(get_buffer_slice_from_accessor(accessor))
                 })
-                .unwrap();
+                .unwrap_or_else(|| panic!("No positions found"));
 
             let normals: Option<&[[f32; 3]]> = first_triangle
                 .attributes()
                 .find(|(semantic, _)| *semantic == gltf::Semantic::Normals)
                 .map(|(_, accessor)| {
                     let data_type = accessor.data_type();
+                    let dimensions = accessor.dimensions();
+                    if dimensions != gltf::accessor::Dimensions::Vec3 {
+                        panic!("Expected vec3 data but found: {:?}", dimensions);
+                    }
+                    if data_type != gltf::accessor::DataType::F32 {
+                        panic!("Expected f32 data but found: {:?}", data_type);
+                    }
+                    bytemuck::cast_slice(get_buffer_slice_from_accessor(accessor))
+                });
+
+            let tex_coords: Option<&[[f32; 2]]> = first_triangle
+                .attributes()
+                .find(|(semantic, _)| *semantic == gltf::Semantic::TexCoords(0))
+                .map(|(_, accessor)| {
+                    let data_type = accessor.data_type();
+                    let dimensions = accessor.dimensions();
+                    if dimensions != gltf::accessor::Dimensions::Vec2 {
+                        panic!("Expected vec2 data but found: {:?}", dimensions);
+                    }
                     if data_type != gltf::accessor::DataType::F32 {
                         panic!("Expected f32 data but found: {:?}", data_type);
                     }
@@ -105,12 +131,15 @@ pub fn load_gltf(path: &str) -> Result<Vec<GltfMesh>> {
             });
 
             GltfMesh {
-                vertices: vertices
+                name: mesh.name().map(String::from),
+                // TODO: use izip! macro from itertools instead of indexing
+                vertices: positions
                     .iter()
                     .enumerate()
                     .map(|(i, position)| GltfVertex {
                         position: *position,
                         normal: normals.and_then(|normals| normals.get(i)).copied(),
+                        tex_coord: tex_coords.and_then(|tex_coords| tex_coords.get(i)).copied(),
                     })
                     .collect(),
                 indices: indices.map(|indices| indices.to_vec()),
@@ -134,23 +163,27 @@ mod tests {
 
     #[test]
     fn test_triangle_without_indices() -> Result<()> {
-        let mesh =
+        let meshes =
             load_gltf("./src/models/gltf/TriangleWithoutIndices/TriangleWithoutIndices.gltf")?;
         assert_eq!(
-            mesh,
+            meshes,
             vec![GltfMesh {
+                name: None,
                 vertices: vec![
                     GltfVertex {
                         position: [0.0, 0.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [1.0, 0.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [0.0, 1.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                 ],
                 indices: None,
@@ -162,22 +195,26 @@ mod tests {
 
     #[test]
     fn test_triangle_with_indices() -> Result<()> {
-        let mesh = load_gltf("./src/models/gltf/Triangle/Triangle.gltf")?;
+        let meshes = load_gltf("./src/models/gltf/Triangle/Triangle.gltf")?;
         assert_eq!(
-            mesh,
+            meshes,
             vec![GltfMesh {
+                name: None,
                 vertices: vec![
                     GltfVertex {
                         position: [0.0, 0.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [1.0, 0.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [0.0, 1.0, 0.0],
                         normal: None,
+                        tex_coord: None,
                     },
                 ],
                 indices: Some(vec![0, 1, 2]),
@@ -189,23 +226,27 @@ mod tests {
 
     #[test]
     fn test_simple_meshes() -> Result<()> {
-        // there's two meshes in this file
-        let mesh = load_gltf("./src/models/gltf/SimpleMeshes/SimpleMeshes.gltf")?;
+        // there's two nodes in this file
+        let meshes = load_gltf("./src/models/gltf/SimpleMeshes/SimpleMeshes.gltf")?;
         assert_eq!(
-            mesh,
+            meshes,
             vec![GltfMesh {
+                name: None,
                 vertices: vec![
                     GltfVertex {
                         position: [0.0, 0.0, 0.0],
                         normal: Some([0.0, 0.0, 1.0]),
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [1.0, 0.0, 0.0],
                         normal: Some([0.0, 0.0, 1.0]),
+                        tex_coord: None,
                     },
                     GltfVertex {
                         position: [0.0, 1.0, 0.0],
                         normal: Some([0.0, 0.0, 1.0]),
+                        tex_coord: None,
                     },
                 ],
                 indices: Some(vec![0, 1, 2]),
@@ -220,9 +261,40 @@ mod tests {
 
     #[test]
     fn test_texture_coordinate() -> Result<()> {
-        // there's two meshes in this file
-        let mesh = load_gltf("./src/models/gltf/TextureCoordinateTest/TextureCoordinateTest.gltf")?;
-        assert_eq!(dbg!(mesh), vec![]);
+        let meshes =
+            load_gltf("./src/models/gltf/TextureCoordinateTest/TextureCoordinateTest.gltf")?;
+        let first_mesh = &meshes[0];
+        // dbg!(first_mesh);
+        assert_eq!(
+            *first_mesh,
+            GltfMesh {
+                name: Some(String::from("TopRightMesh")),
+                vertices: vec![
+                    GltfVertex {
+                        position: [1.2, 0.2000002, -5.2054858e-8,],
+                        normal: Some([-2.1316282e-14, 1.07284414e-7, 1.0,],),
+                        tex_coord: Some([1.0, 0.39999998,],),
+                    },
+                    GltfVertex {
+                        position: [0.19999981, 1.1999999, -1.5933927e-7,],
+                        normal: Some([-2.1316282e-14, 1.07284414e-7, 1.0,],),
+                        tex_coord: Some([0.6, 0.0,],),
+                    },
+                    GltfVertex {
+                        position: [0.19999996, 0.20000005, -5.2054858e-8,],
+                        normal: Some([-2.1316282e-14, 1.07284414e-7, 1.0,],),
+                        tex_coord: Some([0.6, 0.39999998,],),
+                    },
+                    GltfVertex {
+                        position: [1.1999998, 1.2000002, -1.5933927e-7,],
+                        normal: Some([-2.1316282e-14, 1.07284414e-7, 1.0,],),
+                        tex_coord: Some([1.0, 0.0,],),
+                    },
+                ],
+                indices: Some(vec![0, 1, 2, 3, 1, 0,],),
+                instance_transforms: vec![Matrix4::identity()],
+            }
+        );
         Ok(())
     }
 }
