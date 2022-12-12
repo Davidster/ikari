49c69432e1bc8e8025f92557142826763f06cc0e
Andrea Postal <andrea.postal@gmail.com>
2022-12-12 10:06:11 +0100

    Avoid crashes by minimizing to app icon
    Tested on windows 10

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -57,7 +57,7 @@ pub fn run(
                     }
                     // The system is out of memory, we should probably quit
                     Err(wgpu::SurfaceError::OutOfMemory) => {
-                        *control_flow = winit::event_loop::ControlFlow::Exit
+                        *control_flow = ControlFlow::Exit
                     }
                     // All other errors (Outdated, Timeout) should be resolved by the next frame
                     Err(e) => logger.log(&format!("{:?}", e)),
@@ -83,10 +83,14 @@ pub fn run(
                 );
                 match event {
                     WindowEvent::Resized(size) => {
-                        renderer_state.resize(size);
+                        if size.width > 0 && size.height > 0 {
+                            renderer_state.resize(size);
+                        }
                     }
                     WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        renderer_state.resize(*new_inner_size);
+                        if new_inner_size.width > 0 && new_inner_size.height > 0 {
+                            renderer_state.resize(*new_inner_size);
+                        }
                     }
                     WindowEvent::CloseRequested
                     | WindowEvent::KeyboardInput {
@@ -97,7 +101,7 @@ pub fn run(
                                 ..
                             },
                         ..
-                    } => *control_flow = winit::event_loop::ControlFlow::Exit,
+                    } => *control_flow = ControlFlow::Exit,
                     _ => {}
                 };
             }
