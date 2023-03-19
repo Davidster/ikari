Davidster/ikari

commit 1b5172eec445ba867c6badacfd98c33d44d05f64

- support latest version of wgpu - point wgpu to local path to test sampler cache wgpu change - move default texture module into renderer module - add sampler cache

src/gltf_loader.rs
@@ -75,21 +75,20 @@ pub fn build_scene(
                 .unwrap_or((default_sampler.min_filter, default_sampler.mipmap_filter));
 
             Texture::from_decoded_image(
-                &base_renderer_state.device,
-                &base_renderer_state.queue,
+                base_renderer_state,
                 &image_pixels,
                 (image_data.width, image_data.height),
                 texture.name(),
                 texture_format.into(),
                 true,
-                &SamplerDescriptor(wgpu::SamplerDescriptor {
+                &SamplerDescriptor {
                     address_mode_u,
                     address_mode_v,
                     mag_filter,
                     min_filter,
                     mipmap_filter,
                     ..Default::default()
-                }),
+                },
             )
         })
         .collect::<Result<Vec<_>, _>>()?;

Cargo.toml
@@ -46,7 +46,8 @@ obj-rs = "0.7"
 wavefront_obj = "10.0"
 
 # surface
-wgpu = "0.14"
+wgpu = { path = "../wgpu-fork/wgpu" }
+# wgpu = "0.15"
 winit = "0.27"
 
 # math
@@ -63,3 +64,6 @@ oddio = "0.6"
 
 [profile.release]
 #debug = true
+incremental = false
+codegen-units = 1
+debug = true

src/default_textures.rs
@@ -1,51 +1,0 @@
-use super::*;
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-use std::rc::Rc;
-
-pub struct DefaultTextures {
-    textures: HashMap<DefaultTextureType, Rc<Texture>>,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum DefaultTextureType {
-    BaseColor,
-    Normal,
-    MetallicRoughness,
-    MetallicRoughnessGLTF,
-    Emissive,
-    EmissiveGLTF,
-    AmbientOcclusion,
-}
-
-impl DefaultTextures {
-    pub fn new() -> Self {
-        Self {
-            textures: HashMap::new(),
-        }
-    }
-
-    pub fn get_default_texture(
-        &mut self,
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        default_texture_type: DefaultTextureType,
-    ) -> anyhow::Result<Rc<Texture>> {
-        match self.textures.entry(default_texture_type) {
-            Entry::Occupied(texture) => Ok(texture.get().clone()),
-            Entry::Vacant(entry) => {
-                let color: [u8; 4] = match default_texture_type {
-                    DefaultTextureType::BaseColor => [255, 255, 255, 255],
-                    DefaultTextureType::Normal => [127, 127, 255, 255],
-                    DefaultTextureType::MetallicRoughness => [255, 255, 255, 255],
-                    DefaultTextureType::MetallicRoughnessGLTF => [255, 127, 0, 255],
-                    DefaultTextureType::Emissive => [0, 0, 0, 255],
-                    DefaultTextureType::EmissiveGLTF => [255, 255, 255, 255],
-                    DefaultTextureType::AmbientOcclusion => [255, 255, 255, 255],
-                };
-                let texture = Rc::new(Texture::from_color(device, queue, color)?);
-                Ok(entry.insert(texture).clone())
-            }
-        }
-    }
-}

src/game.rs
@@ -327,30 +327,43 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
     let earth_texture_path = "./src/textures/8k_earth.jpg";
     let earth_texture_bytes = std::fs::read(earth_texture_path)?;
     let earth_texture = Texture::from_encoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &earth_texture_bytes,
         earth_texture_path,
         None,
         true,
-        &Default::default(),
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
 
     let earth_normal_map_path = "./src/textures/8k_earth_normal_map.jpg";
     let earth_normal_map_bytes = std::fs::read(earth_normal_map_path)?;
     let earth_normal_map = Texture::from_encoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &earth_normal_map_bytes,
         earth_normal_map_path,
         wgpu::TextureFormat::Rgba8Unorm.into(),
         false,
-        &Default::default(),
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
 
     let test_object_metallic_roughness_map = Texture::from_color(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         [
             255,
             (0.12 * 255.0f32).round() as u8,
@@ -430,17 +443,21 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
         img
     };
     let big_checkerboard_texture = Texture::from_decoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &big_checkerboard_texture_img,
         big_checkerboard_texture_img.dimensions(),
         Some("big_checkerboard_texture"),
         None,
         true,
-        &texture::SamplerDescriptor(wgpu::SamplerDescriptor {
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Nearest,
-            ..texture::SamplerDescriptor::default().0
-        }),
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
 
     let small_checkerboard_texture_img = {
@@ -464,17 +481,21 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
         img
     };
     let small_checkerboard_texture = Texture::from_decoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &small_checkerboard_texture_img,
         small_checkerboard_texture_img.dimensions(),
         Some("small_checkerboard_texture"),
         None,
         true,
-        &texture::SamplerDescriptor(wgpu::SamplerDescriptor {
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Nearest,
-            ..texture::SamplerDescriptor::default().0
-        }),
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
 
     // add balls to scene
@@ -483,13 +504,20 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
     let mars_texture_path = "./src/textures/8k_mars.jpg";
     let mars_texture_bytes = std::fs::read(mars_texture_path)?;
     let mars_texture = Texture::from_encoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &mars_texture_bytes,
         mars_texture_path,
         None,
         true,
-        &Default::default(),
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
 
     let ball_count = 0;
@@ -678,19 +706,21 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
     #[allow(unused_assignments)]
     let mut crosshair_node_id: Option<GameNodeId> = None;
     let crosshair_texture = Texture::from_decoded_image(
-        &renderer_state.base.device,
-        &renderer_state.base.queue,
+        &mut renderer_state.base,
         &crosshair_texture_img,
         crosshair_texture_img.dimensions(),
         Some("crosshair_texture"),
         None,
         false,
-        &texture::SamplerDescriptor(wgpu::SamplerDescriptor {
-            // mag_filter: wgpu::FilterMode::Nearest,
-            // min_filter: wgpu::FilterMode::Nearest,
-            // mipmap_filter: wgpu::FilterMode::Nearest,
-            ..texture::SamplerDescriptor::default().0
-        }),
+        &SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        },
     )?;
     let crosshair_quad = BasicMesh {
         vertices: vec![[1.0, 1.0], [1.0, -1.0], [-1.0, -1.0], [-1.0, 1.0]]
@@ -708,19 +738,14 @@ pub fn init_game_state(mut scene: Scene, renderer_state: &mut RendererState) -> 
             .collect(),
         indices: vec![0, 2, 1, 0, 3, 2],
     };
+    let crosshair_ambient_occlusion = Texture::from_color(&mut renderer_state.base, [0, 0, 0, 0])?;
+    let crosshair_metallic_roughness =
+        Texture::from_color(&mut renderer_state.base, [0, 0, 255, 0])?;
     let crosshair_mesh_index = renderer_state.bind_basic_pbr_mesh(
         &crosshair_quad,
         &PbrMaterial {
-            ambient_occlusion: Some(&Texture::from_color(
-                &renderer_state.base.device,
-                &renderer_state.base.queue,
-                [0, 0, 0, 0],
-            )?),
-            metallic_roughness: Some(&Texture::from_color(
-                &renderer_state.base.device,
-                &renderer_state.base.queue,
-                [0, 0, 255, 0],
-            )?),
+            ambient_occlusion: Some(&crosshair_ambient_occlusion),
+            metallic_roughness: Some(&crosshair_metallic_roughness),
             base_color: Some(&crosshair_texture),
             emissive: Some(&crosshair_texture),
             ..Default::default()

src/main.rs
@@ -5,7 +5,6 @@ mod buffer;
 mod camera;
 mod character;
 mod collisions;
-mod default_textures;
 mod game;
 mod game_state;
 mod gameloop;
@@ -19,6 +18,7 @@ mod physics_ball;
 mod player_controller;
 mod renderer;
 mod revolver;
+mod sampler_cache;
 mod scene;
 mod scene_tree;
 mod skinning;
@@ -33,7 +33,6 @@ use buffer::*;
 use camera::*;
 use character::*;
 use collisions::*;
-use default_textures::*;
 use game::*;
 use game_state::*;
 use gltf_loader::*;
@@ -47,6 +46,7 @@ use player_controller::*;
 use rapier3d::prelude::*;
 use renderer::*;
 use revolver::*;
+use sampler_cache::*;
 use scene::*;
 use skinning::*;
 use texture::*;
@@ -77,7 +77,8 @@ async fn start() {
         let backends = if cfg!(target_os = "linux") {
             wgpu::Backends::from(wgpu::Backend::Vulkan)
         } else {
-            wgpu::Backends::all()
+            // wgpu::Backends::from(wgpu::Backend::Dx12)
+            wgpu::Backends::from(wgpu::Backend::Dx12)
         };
         BaseRendererState::new(&window, backends, wgpu::PresentMode::AutoNoVsync).await
     };

src/sampler_cache.rs
@@ -1,0 +1,94 @@
+use wgpu::{Device, Sampler};
+
+// same as wgpu::SamplerDescriptor but without the label
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct SamplerDescriptor {
+    pub address_mode_u: wgpu::AddressMode,
+    pub address_mode_v: wgpu::AddressMode,
+    pub address_mode_w: wgpu::AddressMode,
+    pub mag_filter: wgpu::FilterMode,
+    pub min_filter: wgpu::FilterMode,
+    pub mipmap_filter: wgpu::FilterMode,
+    pub lod_min_clamp: f32,
+    pub lod_max_clamp: f32,
+    pub compare: Option<wgpu::CompareFunction>,
+    pub anisotropy_clamp: Option<std::num::NonZeroU8>,
+    pub border_color: Option<wgpu::SamplerBorderColor>,
+}
+
+impl Default for SamplerDescriptor {
+    fn default() -> Self {
+        let def = wgpu::SamplerDescriptor::default();
+        Self {
+            address_mode_u: def.address_mode_u,
+            address_mode_v: def.address_mode_v,
+            address_mode_w: def.address_mode_w,
+            mag_filter: def.mag_filter,
+            min_filter: def.min_filter,
+            mipmap_filter: def.mipmap_filter,
+            lod_min_clamp: def.lod_min_clamp,
+            lod_max_clamp: def.lod_max_clamp,
+            compare: def.compare,
+            anisotropy_clamp: def.anisotropy_clamp,
+            border_color: def.border_color,
+        }
+    }
+}
+
+impl SamplerDescriptor {
+    pub fn into_wgpu(self) -> wgpu::SamplerDescriptor<'static> {
+        wgpu::SamplerDescriptor {
+            label: None,
+            address_mode_u: self.address_mode_u,
+            address_mode_v: self.address_mode_v,
+            address_mode_w: self.address_mode_w,
+            mag_filter: self.mag_filter,
+            min_filter: self.min_filter,
+            mipmap_filter: self.mipmap_filter,
+            lod_min_clamp: self.lod_min_clamp,
+            lod_max_clamp: self.lod_max_clamp,
+            compare: self.compare,
+            anisotropy_clamp: self.anisotropy_clamp,
+            border_color: self.border_color,
+        }
+    }
+}
+
+pub struct SamplerCache {
+    samplers: Vec<(SamplerDescriptor, Sampler)>,
+}
+
+impl SamplerCache {
+    pub fn new() -> Self {
+        Self { samplers: vec![] }
+    }
+
+    pub fn get_sampler_index(
+        &mut self,
+        device: &Device,
+        a_descriptor: &SamplerDescriptor,
+    ) -> usize {
+        if let Some(existing_sampler_index) = self
+            .samplers
+            .iter()
+            .enumerate()
+            .find_map(|(i, (descriptor, _))| descriptor.eq(a_descriptor).then_some(i))
+        {
+            return existing_sampler_index;
+        }
+        let new_sampler_index = self.samplers.len();
+        let new_sampler = device.create_sampler(&a_descriptor.into_wgpu());
+        self.samplers.push((*a_descriptor, new_sampler));
+        new_sampler_index
+    }
+
+    pub fn get_sampler_by_index(&self, sampler_index: usize) -> &Sampler {
+        let (_, sampler) = &self.samplers[sampler_index];
+        sampler
+    }
+
+    pub fn get_sampler(&mut self, device: &Device, a_descriptor: &SamplerDescriptor) -> &Sampler {
+        let sampler_index = self.get_sampler_index(device, a_descriptor);
+        self.get_sampler_by_index(sampler_index)
+    }
+}

src/shaders/blit.wgsl
@@ -105,7 +105,7 @@ fn bloom_blur_fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
     let tex_dimension_f32 = vec2<f32>(f32(tex_dimensions.x), f32(tex_dimensions.y));
     let tex_offset = 1.0 / tex_dimension_f32;
     var result = textureSample(texture_1, sampler_1, in.tex_coords).rgb * gaussian_blur_weights[0];
-    if (bloom_config.direction == 0.0) {
+    if bloom_config.direction == 0.0 {
         for (var i = 1; i < 5; i = i + 1) {
             result = result + textureSample(
                 texture_1,
@@ -137,10 +137,10 @@ fn bloom_blur_fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
 
 // BRDF LUT:
 
-let pi: f32 = 3.141592653589793;
-let two_pi: f32 = 6.283185307179586;
-let half_pi: f32 = 1.570796326794897;
-let epsilon: f32 = 0.00001;
+const pi: f32 = 3.141592653589793;
+const two_pi: f32 = 6.283185307179586;
+const half_pi: f32 = 1.570796326794897;
+const epsilon: f32 = 0.00001;
 
 fn geometry_func_schlick_ggx_k_direct(
     a: f32,
@@ -189,7 +189,7 @@ fn importance_sampled_ggx(x_i: vec2<f32>, n: vec3<f32>, a: f32) -> vec3<f32> {
     );
 
     var up: vec3<f32>;
-    if (abs(n.z) < 0.999) {
+    if abs(n.z) < 0.999 {
         up = vec3<f32>(0.0, 0.0, 1.0);
     } else {
         up = vec3<f32>(1.0, 0.0, 0.0);
@@ -259,7 +259,7 @@ fn integrate_brdf(n_dot_v: f32, roughness: f32) -> vec2<f32> {
         let n_dot_h = max(h.z, 0.0);
         let v_dot_h = max(dot(v, h), 0.0);
 
-        if (n_dot_l > 0.0) {
+        if n_dot_l > 0.0 {
             let k = geometry_func_schlick_ggx_k_ibl(roughness); // 0.41
             let g = geometry_func_smith_ggx(k, n, v, l); // 0.95
             let g_vis = (g * v_dot_h) / (n_dot_h * n_dot_v); // 0.956

commit 32a90113ef4dba96c814c7f6211d7d780e1838a5

fix old regression from 1b5172eec445ba where sampler default changed, causing nearest neighbor filtering on normal map

src/gltf_loader.rs
@@ -372,7 +372,11 @@ fn get_textures(
             };
 
             let gltf_sampler = texture.sampler();
-            let default_sampler = SamplerDescriptor::default();
+            let default_sampler = SamplerDescriptor {
+                min_filter: wgpu::FilterMode::Linear,
+                mag_filter: wgpu::FilterMode::Linear,
+                ..Default::default()
+            };
             let address_mode_u = sampler_wrapping_mode_to_wgpu(gltf_sampler.wrap_s());
             let address_mode_v = sampler_wrapping_mode_to_wgpu(gltf_sampler.wrap_t());
             let mag_filter = gltf_sampler
@@ -382,6 +386,7 @@ fn get_textures(
                     gltf::texture::MagFilter::Linear => wgpu::FilterMode::Linear,
                 })
                 .unwrap_or(default_sampler.mag_filter);
+
             let (min_filter, mipmap_filter) = gltf_sampler
                 .min_filter()
                 .map(|gltf_min_filter| match gltf_min_filter {