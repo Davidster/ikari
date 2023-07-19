12671dd4e28b0c9a4bef2adec5eebf4c0c68f31d
David Huculak <davidhuculak5@gmail.com>
2023-07-18 21:54:05 -0400

    set RUST_LOG and RUST_BACKTRACE env vars automatically if they are not defined

--- ikari/src/example_game/main.rs
+++ ikari/src/example_game/main.rs
@@ -78,8 +78,27 @@ async fn start() {
     }
 }
 
+fn env_var_is_defined(var: &str) -> bool {
+    match std::env::var(var) {
+        Ok(val) => !val.is_empty(),
+        Err(_) => false,
+    }
+}
+
 fn main() {
-    env_logger::init();
+    if !env_var_is_defined("RUST_BACKTRACE") {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+
+    if env_var_is_defined("RUST_LOG") {
+        env_logger::init();
+    } else {
+        env_logger::builder()
+            .filter(Some(env!("CARGO_PKG_NAME")), log::LevelFilter::Info)
+            .filter(Some(env!("CARGO_BIN_NAME")), log::LevelFilter::Info)
+            .filter(Some("wgpu"), log::LevelFilter::Warn)
+            .init();
+    }
 
     #[cfg(feature = "tracy-n-alloc")]
     {
