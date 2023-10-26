5784269f31523af1f4c63e6adb867cb04e6971cd
David Huculak <davidhuculak5@gmail.com>
2023-10-25 20:03:45 -0400

    add path to error messages for web

--- ikari/src/file_manager.rs
+++ ikari/src/file_manager.rs
@@ -69,8 +69,11 @@ mod native {
 #[cfg(target_arch = "wasm32")]
 mod web {
     use wasm_bindgen::prelude::*;
+    use wasm_bindgen_futures::JsFuture;
 
     use crate::file_manager::{FileManager, GameFilePath};
+
+    const ASSET_SERVER: &str = "http://localhost:8000"; // TODO: make the asset server be configurable by the game
 
     #[wasm_bindgen]
     extern "C" {
@@ -83,29 +86,31 @@ mod web {
         fn worker(this: &Global) -> JsValue;
     }
 
-    async fn run_promise(promise: js_sys::Promise) -> anyhow::Result<JsValue> {
-        map_js_err(wasm_bindgen_futures::JsFuture::from(promise).await)
-    }
-
-    fn map_js_err<T>(result: std::result::Result<T, JsValue>) -> anyhow::Result<T> {
-        result.map_err(|err| anyhow::anyhow!(err.as_string().unwrap_or_default()))
-    }
-
-    // TODO: add the path to all error messages here?
     impl FileManager {
         pub async fn read(path: &GameFilePath) -> anyhow::Result<Vec<u8>> {
-            const ASSET_SERVER: &str = "http://localhost:8000";
+            let resolved_path = path.resolve();
+            let get_request_url = || format!("{ASSET_SERVER}/{}", resolved_path.display());
+            Self::read_internal(get_request_url())
+                .await
+                .map(|js_value| js_sys::Uint8Array::new(&js_value).to_vec())
+                .map_err(|err| {
+                    anyhow::anyhow!(
+                        "Error reading from url {}:\n{}",
+                        get_request_url(),
+                        err.as_string().unwrap_or_default()
+                    )
+                })
+        }
 
+        async fn read_internal(url: String) -> std::result::Result<JsValue, JsValue> {
             use web_sys::{Blob, RequestInit, Response};
 
             let mut opts = RequestInit::new();
             opts.method("GET");
-            let path = path.resolve();
-            let url = format!("{ASSET_SERVER}/{}", path.display());
-            let request = map_js_err(web_sys::Request::new_with_str_and_init(&url, &opts))?;
+            let request = web_sys::Request::new_with_str_and_init(&url, &opts)?;
 
             let global: Global = js_sys::global().unchecked_into();
-            let resp_value = run_promise(if !global.window().is_undefined() {
+            let resp_value = JsFuture::from(if !global.window().is_undefined() {
                 global
                     .unchecked_into::<web_sys::Window>()
                     .fetch_with_request(&request)
@@ -114,7 +119,7 @@ mod web {
                 global_scope.fetch_with_request(&request)
             } else {
                 panic!(
-                    "read function is only supported on the main thread or from a dedicated worker"
+                    "this function is only supported on the main thread or from a dedicated worker"
                 );
             })
             .await?;
@@ -122,13 +127,15 @@ mod web {
             let resp: Response = resp_value.dyn_into().unwrap();
             if !resp.ok() {
                 let status = resp.status();
-                anyhow::bail!("Request to {url} responded with error status code: {status}")
+                return std::result::Result::Err(JsValue::from(format!(
+                    "Request to {url} responded with error status code: {status}"
+                )));
             }
 
-            let blob = run_promise(map_js_err(resp.blob())?).await?;
-            let array_buffer: JsValue = run_promise(Blob::from(blob).array_buffer()).await?;
+            let blob = JsFuture::from(resp.blob()?).await?;
+            let array_buffer: JsValue = JsFuture::from(Blob::from(blob).array_buffer()).await?;
 
-            anyhow::Ok(js_sys::Uint8Array::new(&array_buffer).to_vec())
+            std::result::Result::Ok(array_buffer)
         }
 
         pub async fn read_to_string(path: &GameFilePath) -> anyhow::Result<String> {
